a3821890e52bef26c5be4d3ab3b14877b3a53cfa
Bernhard Reitinger <bernhard@reitinger.eu>
2024-12-18 18:52:20 +0100

    Initial neovim config

--- config/init.lua
+++ config/init.lua
@@ -1,10 +1,2 @@
---- This is the lua-base init file
----
---[[
-This is a multi-line comment
---]]
-
--- Leader key is SPACE
-vim.g.mapleader = ' '
-
-print "nvim config loaded!"
+require("config.lazy")
+require("options")
---
+++ config/lua/options.lua
@@ -0,0 +1,6 @@
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
+
+
+-- Keymaps
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
