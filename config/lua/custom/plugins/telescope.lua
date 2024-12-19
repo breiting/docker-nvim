7338e519e2586a4ea7bc217102bfbe1616d83b24
Bernhard Reitinger <bernhard@reitinger.eu>
2024-12-19 18:36:05 +0100

    Improve telescope

--- config/lua/custom/plugins/telescope.lua
+++ config/lua/custom/plugins/telescope.lua
@@ -13,7 +13,12 @@ return {
         pickers = {
           find_files = {
             theme = "ivy"
-          }
+          },
+          buffers = {
+            theme = "ivy",
+            sort_mru = true,
+            ignore_current_buffer = true
+          },
         },
 
         extensions = {
@@ -21,10 +26,13 @@ return {
         }
       }
 
+      local builtin = require('telescope.builtin')
+
       require('telescope').load_extension('fzf')
 
-      vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)
-      vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files)
+      vim.keymap.set("n", "<space>fh", builtin.help_tags)
+      vim.keymap.set("n", "<space>ff", builtin.find_files)
+      vim.keymap.set("n", "<leader><Esc>", builtin.buffers)
       vim.keymap.set("n", "<space>en", function()
         require('telescope.builtin').find_files {
           cwd = vim.fn.stdpath("config")
